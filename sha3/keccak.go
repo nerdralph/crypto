f89978bc22fed848937eedf12296b2d7879b86e3
Nerd Ralph <ralphdoncaster@gmail.com>
2018-03-09 00:19:16 +0000

    add zero-copy re-hashing

--- sha3/keccak.go
+++ sha3/keccak.go
@@ -8,6 +8,7 @@ package sha3
 
 import (
 	"hash"
+	"unsafe"
 )
 
 // Creates a new Keccak-256 hash.
@@ -31,3 +32,38 @@ func SumK512(data []byte) (digest [64]byte) {
 	h.Sum(digest[:0])
 	return
 }
+
+// for repetitive hashing using the digest output of one hash as the input
+// for the next hash
+type ReHash interface {
+	Hash()
+	Data() []byte
+}
+
+func (s* state) Data() []byte {
+	// statte array is first struct member and has the same address
+	arr := (*[64]byte)(unsafe.Pointer(s))
+	return arr[:s.outputLen]
+}
+
+// keccakF1600
+func (s* state) Hash() {
+	//st := (*[25]uint64)(unsafe.Pointer(h.State))
+	st := (*[25]uint64)(unsafe.Pointer(s))
+	pos := s.outputLen/8
+	st[pos] = uint64(s.dsbyte)
+	// zero the rest of the state
+	for i := pos+1; i < 25; i++ {
+		st[i] = 0
+	}
+	// add pad bit
+	st[25 - 2*pos -1] ^= 1<<63
+	keccakF1600(st)
+}
+
+// Creates a new Keccak-256 ReHash.
+func ReHashK256() ReHash { return &state{outputLen: 32, dsbyte: 0x01} }
+
+// Creates a new Keccak-512 ReHash.
+func ReHashK512() ReHash { return &state{outputLen: 64, dsbyte: 0x01} }
+
